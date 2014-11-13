4aeff9f1e4d93c7d6ee168fdc1f415e50628487e
yacc <origingod.oh@gmail.com>
2014-11-13 10:36:01 +0800

    pack

--- log_shipper.py
+++ log_shipper.py
@@ -28,6 +28,22 @@ class LogShipper(object):
             return -1
         return ret
 
+
 if __name__=='__main__':
-    ls = LogShipper()
-    ls.ship({'log_type':'ossec,hello'})
+    from parse_ossec import LogParse
+    lp = LogParser(log_file='/var/ossec/logs/alerts/alerts.log')
+    ls = LogShipper(rhost='127.0.0.1',port=6379)
+
+    alert_text = lp.get_one_log()
+    while 1:
+        if alert_text == '' or alert_text == ' ':
+            # EOF
+            break
+        else:
+            alert_dict = lp.parse_one_log(alert_text)
+            alert_text = lp.get_one_log()
+            if alert_dict == None:
+                continue
+            ls.ship(alert_dict)
+
+    print("parse and ship done.")
